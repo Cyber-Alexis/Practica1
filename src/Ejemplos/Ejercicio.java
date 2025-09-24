3a74095d36c3b83e2d3f636e66d3f38325418d8b
Cyber-Alexis <alexis.n.buza@alumnes.ilerna.com>
2025-09-24 18:57:45 +0200

    Un cambio en la rama main

--- src/Ejemplos/Ejercicio.java
+++ src/Ejemplos/Ejercicio.java
@@ -1,10 +1,10 @@
 package Ejemplos;
-
+import java.util.Scanner;
 public class Ejercicio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+//Hola mundo
+		
+	}	
 }
